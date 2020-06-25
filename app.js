21a2f6bac0b7854ae53b095ef591647e52bd99cb
rohan <rohanrajgautam@gmail.com>
2020-06-25 12:26:35 +0545

    styled campground and landing page

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ app.set('view engine', 'ejs');
 
 //campground array
 let campgrounds = [
-  { name: 'Simon Creek', image: 'https://image.shutterstock.com/image-photo/camping-woods-shenandoah-mountain-bonfire-260nw-1106316416.jpg' },
+  { name: 'Simon Creek', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-74-768x513.jpg' },
+  { name: 'Tom Hank', image: 'https://media.gettyimages.com/photos/camping-in-a-tent-under-the-stars-and-milky-way-galaxy-picture-id904960682?s=2048x2048' },
+  { name: 'Robert Niro', image: 'https://media.gettyimages.com/photos/young-people-camping-with-a-baby-girl-picture-id627343204?s=2048x2048' },
+  { name: 'Simon Creek', image: 'https://www.elacampground.com/wp-content/uploads/2019/06/Ela-Campground-74-768x513.jpg' },
   { name: 'Tom Hank', image: 'https://media.gettyimages.com/photos/camping-in-a-tent-under-the-stars-and-milky-way-galaxy-picture-id904960682?s=2048x2048' },
   { name: 'Robert Niro', image: 'https://media.gettyimages.com/photos/young-people-camping-with-a-baby-girl-picture-id627343204?s=2048x2048' },
 ]
